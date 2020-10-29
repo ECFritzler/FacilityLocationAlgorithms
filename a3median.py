24c765cf0aaec980fe3501b6e9f5829b9c4b002b
Claire Fritzler <claire@tribot.org>
2020-10-29 10:44:45 -0700

    Update to indtructions
    The instructions were clarified

--- a3median.py
+++ a3median.py
@@ -1,7 +1,7 @@
 #######################################################################################
 # a3median.py
 # Claire Fritzler - 001167579
-# 4110 Assignment 3
+#
 #
 # The a3median.py program should:
 # - Take an optional parameter -n value
@@ -15,22 +15,22 @@
 import networkx as nx
 import random
 import argparse
-import itertools 
+import itertools
 # Function to initialize the graph with n nodes and write the graph to a GML file
 def initGraph(n, f):
 	Graph = nx.path_graph(n)
-	
+
     #Initialize weights
     for i in range(0, n):
         weight = random.randint(1, 20)
         Graph.node[i]['weight'] = weight
         Graph.node[i]['index'] = i
-	
-	#initialize lengths		
+
+	#initialize lengths
 	for i in range(1, n):
 		length = random.randint(1, 10)
 		Graph.edges[i-1, i]['length'] = length
-		
+
 	f = f + ".gml"
 	nx.write_gml(Graph, f)
 	return Graph
@@ -40,13 +40,13 @@ def initGraph(n, f):
 def isPath(Graph):
 	edges = nx.number_of_edges(Graph)
 	nodes = nx.number_of_nodes(Graph)
-	
+
 	if((nodes - edges) != 1):
 		return False
 	else:
 		return True
-		
-# Function to check if the nodes all have weights		
+
+# Function to check if the nodes all have weights
 def haveWeights(Graph):
 	for i in Graph.nodes():
 		if(not Graph.node[i]['weight']):
@@ -65,8 +65,8 @@ def haveLengths(Graph):
 			print("Your graph has invalid edge lengths")
 			return False
 	return True
-	
-# Parse the input user arguments 	
+
+# Parse the input user arguments
 parser = argparse.ArgumentParser(description='1-Median Calculator for a Graph')
 parser.add_argument('-n', nargs='?', type=int, help='Number of nodes')
 parser.add_argument('file', type=str, help='GML File')
@@ -75,11 +75,11 @@ args = parser.parse_args()
 n = args.n
 file = args.file
 p = args.p
-	
-	
+
+
 ############################################################################################
 # 1-Median Calculation:
-# Calculate the cost of each node and store it in a list 
+# Calculate the cost of each node and store it in a list
 # Go through the list and find the minimum value
 ############################################################################################
 
@@ -92,7 +92,7 @@ p = args.p
 #			distance = nx.shortest_path_length(Graph, customer, facility, 'length')
 #			sum = sum + (weight + distance)
 #	costs.append((sum, facility))
-#	
+#
 #optimalFacility = min(costs, key=lambda t: (t[0]))
 # print("The cost of the 1-Median is: " + str(optimalFacility[0]))
 # print("The ID of the 1-Median is: " + str(optimalFacility[1]))
@@ -106,7 +106,7 @@ p = args.p
 
 # Function to calculate the weighted distance of two vertices
 # Take the weight of the first vertex multiplied by the distance to the second vertex
-	
+
 F = [list() for x in range(n)]
 G = []
 row = []
@@ -114,7 +114,7 @@ def PMedian(Graph):
 	for q in range(1, p): #Iterate through the number of facilities
 		sum = 0
 		cost = []
-		for j in range(1, n): #Iterate through the number of nodes 
+		for j in range(1, n): #Iterate through the number of nodes
 			# Compute G
 			for k in range(j+1, n+1): #from the current node to the number of nodes
 				if(q == 1):
@@ -130,17 +130,17 @@ def PMedian(Graph):
 							weight = Graph.node[t]['weight']
 							distance = nx.shortest_path_length(Graph, t, j, 'length')
 							sum = sum + (weight + distance)
-							cost.append(sum + F[q-1][k])				
+							cost.append(sum + F[q-1][k])
 							G[q].append(cost)
 			for k in range(j, n):
-				for t in range(j, k):	
+				for t in range(j, k):
 					if(j != t):
 						weight = Graph.node[t]['weight']
 						distance = nx.shortest_path_length(Graph, t, k, 'length')
-						sum = sum + (weight + distance)							
-						cost.append(sum + G[q][k]) 
+						sum = sum + (weight + distance)
+						cost.append(sum + G[q][k])
 						F[q].append(cost)
-print(G)					
+print(G)
 
 
 
@@ -157,12 +157,12 @@ else:
 	print("A random graph with " + str(n) + " nodes will be generated for you")
 	Graph = initGraph(n, file)
 	PMedian(Graph)
-	
+
 
 if(not isPath(Graph)):
 	print("Your graph is not a simple path")
 	exit()
-	
+
 if(not haveWeights(Graph)):
 	print("Your graph has invalid weights")
 	exit()
@@ -178,12 +178,12 @@ if(not haveLengths(Graph)):
 #		for j in range(1, n):
 #			for t in range(j to n):
 #				sum = sum + d(vt, vk)
-#	for j in range(1, n): 
+#	for j in range(1, n):
 #		for k in range(j, n):
 #			for t in range(j, k):
 #				sum = d(vt, vk) + G(q,k)
-				
-#				
+
+#
 #def G(q, j):
 #	if (q == 1):
 #		for j in range(1, n):
@@ -192,13 +192,4 @@ if(not haveLengths(Graph)):
 #	for j in range(1, n):
 #		for k in range((j+1), (n+1)):
 #			for t in range(j, (k-1)):
-#				sum = d(vt, vj) + F(q-1, k) 
-
-
-
-
-
-
-
-
-
+#				sum = d(vt, vj) + F(q-1, k)
